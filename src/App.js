ca863c7a7f62cc0b32c6e1034178efe2249a8d7c
Juan Ruiz <67602591+juancaruizc@users.noreply.github.com>
2022-07-01 22:45:00 +0000

    completed tasks without styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import Character from './components/Character.js'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +10,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characterData, setCharacterData] = useState([])
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setCharacterData(res.data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characterData.map((character, idx) => {
+      return <Character key={idx} starWarsData={character}/>
+      })}
+    
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
-// Write your Character component here
+import React from 'react'
+
+function Character(props){
+  const {starWarsData} = props
+  
+  return(
+    <div>
+      <h2>{starWarsData.name}</h2>
+      <h3>{starWarsData.birth_year}</h3>
+    </div>
+  )
+}
+
+export default Character
